9c49f9c78c97ddd1b93f22275f8cdd407228d56c
Luke Kaalim <lukekaalim@gmail.com>
2019-09-17 15:28:54 +1000

    Add models and disposables

--- src/contract.js
+++ src/contract.js
@@ -1,5 +1,7 @@
 // @flow strict
+const { nameModel, modelTuple, modelArray, modelObject, stringModel, numberModel, modelTagUnion } = require('@lukekaalim/model');
 /*::
+import type { Model } from '@lukekaalim/model';
 export type HTTPMethod =
   | 'GET'
   | 'POST'
@@ -8,19 +10,30 @@ export type HTTPMethod =
   | 'UPDATE'
 */
 
+const httpMethodModel/*: Model<HTTPMethod>*/ = nameModel(
+  'Contract/HTTPMethod',
+  modelTagUnion(['GET', 'POST', 'DELETE', 'PUT', 'UPDATE']),
+);
+
 /*::
 export type Request = {
   path: string,
   headers: Array<[string, string]>,
   method: HTTPMethod,
-  body: string | null,
+  body: string,
 };
 */
+const requestModel/*: Model<Request>*/ = nameModel('Contract/Request', modelObject({
+  path: stringModel,
+  headers: modelArray(modelTuple([stringModel, stringModel])),
+  method: httpMethodModel,
+  body: stringModel,
+}));
 
 const createRequest = (
   path/*: string*/,
   method/*: HTTPMethod*/ = 'GET',
-  body/*: string | null*/ = null,
+  body/*: string*/ = '',
   headers/*: Array<[string, string]>*/ = [],
 )/*: Request */ => ({
   path,
@@ -36,6 +49,11 @@ export type Response = {
   body: string,
 };
 */
+const responseModel/*: Model<Response>*/ = nameModel('Contract/Response', modelObject({
+  status: numberModel,
+  headers: modelArray(modelTuple([stringModel, stringModel])),
+  body: stringModel,
+}));
 const createResponse = (
   body/*: string*/,
   status/*: number*/ = 200,
@@ -53,19 +71,25 @@ export type Contract = {
   response: Response,
 };
 */
+const contractModel/*: Model<Contract>*/ = nameModel('Contract', modelObject({
+  name: stringModel,
+  request: requestModel,
+  response: responseModel,
+}));
 
 const createContract = (
   name/*: string*/,
-  request/*:Request*/,
+  request/*: Request*/,
   response/*: Response*/
 )/*: Contract*/ => ({
-  name,
-  request,
-  response,
+  name, request, response
 });
 
 module.exports = {
   createContract,
   createRequest,
   createResponse,
-}
+  contractModel,
+  requestModel,
+  responseModel,
+};
